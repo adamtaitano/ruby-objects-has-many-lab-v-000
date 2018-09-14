3e340315108175c90b6186c00634155df1ec4e32
Adam Taitano <adam.taitano@gmail.com>
2018-09-14 02:13:58 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,7 +4,7 @@ class Artist
 
   def initialize(name)
     @name = name
-    @songs = []
+    @songs.clear
   end
 
   def songs
@@ -13,18 +13,16 @@ class Artist
 
   def add_song(song)
     @songs << song
-    @@song_count += 1
     song.artist = self
   end
 
   def add_song_by_name(name)
     song = Song.new(name)
-    @@song_count += 1
     @songs << song
     song.artist = self
   end
 
   def self.song_count
-    @@song_count
+    @songs.size
   end
 end
