a416bc6e64185bc57999315bd8a74c2fe926510c
Adam Taitano <adam.taitano@gmail.com>
2018-09-14 02:19:45 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,6 @@
 class Artist
   attr_accessor :name
-  @@songs_count = 0
+  @@song_count = 0
   
   def initialize(name)
     @name = name
@@ -13,18 +13,18 @@ class Artist
 
   def add_song(song)
     @songs << song
-    @@songs_count += 1
+    @@song_count += 1
     song.artist = self
   end
 
   def add_song_by_name(name)
     song = Song.new(name)
     @songs << song
-    @@songs_count += 1
+    @@song_count += 1
     song.artist = self
   end
 
   def self.song_count
-    @@songs_count
+    @@song_count
   end
 end
